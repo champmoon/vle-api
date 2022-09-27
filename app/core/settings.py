eb0e0943722720872cba3a1e3d484b6e95369ac8
champmoon <antipov8v@gmail.com>
2022-09-27 18:49:31 +0300

    add settings for db connection

---
+++ app/core/settings.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from pydantic import BaseSettings, PostgresDsn, validator
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+
+    SQLALCHEMY_DATABASE_URI: PostgresDsn | None = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: str | None, values: dict[str, str]) -> str:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOST") or "",
+            port=values.get("POSTGRES_PORT"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    @classmethod
+    def from_env(cls) -> Settings:
+        return cls.parse_obj({})
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings.from_env()
