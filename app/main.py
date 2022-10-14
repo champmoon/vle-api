675a2a4fa264c5d839b533546291b2060d87f7c6
champmoon <antipov8v@gmail.com>
2022-10-14 14:12:24 +0300

    create static files

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.api.api_v1.api import api_router
 
 app = FastAPI(
     title="vle-api", description="Virtual Learning Environment API", version="0.1.0"
 )
+
+app.mount("/storage", StaticFiles(directory="storage"), name="storage")
 
 app.include_router(api_router)
